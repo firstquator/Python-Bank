57584ac8d532db76a803652597738b3453786d29
LeeSangHyeon <tkdgus4693@naver.com>
2022-04-13 22:05:07 +0900

    Make display function and Create inquiry method

--- bank.py
+++ bank.py
@@ -2,6 +2,10 @@ from curses.ascii import isdigit
 from math import floor
 from re import search
 
+def display_user_info(user: dict, sep: str):
+  for key, value in user:
+    print(f'{key} : {value} {sep}')
+
 class Bank():
   def __init__(self):
     self.users = []
@@ -24,13 +28,13 @@ class Bank():
       name = input("이름 : ")
 
     # Error : 잘못된 deposit 형식 전달
-    while not money.isdigit() or floor(int(money)) < 0:
+    while not money.isdigit() or int(money) < 0:
       print("입금하신 금액이 올바르지 않습니다 : 음수 입력\n")
       money = int(input("예금 : "))
 
     user = {"Account" : account,
             "Name" : name,
-            "Balance": floor(int(money))
+            "Balance": int(money)
             }
 
     self.users.append(user)
@@ -41,32 +45,28 @@ class Bank():
   # =================== 입금 method ===================
   def deposit(self):
     print("======입금 하기======")
-    account = input("입금하실 계좌번호를 입력해주세요 : ")
 
+    account = input("입금하실 계좌번호를 입력해주세요 : ")
     # Error : 잘못된 account 형식 전달
     while not account.isdigit():
       print("잘못된 형식의 계좌번호입니다.")
       account = input("계좌번호 : ")
 
     user = (user for user in self.users if user['Account'] == account)
-
     # Error : users dict 내 찾는 user가 없는 경우
     if user == None:
       print("해당 계좌번호를 가진 사용자가 없습니다.")
       return-1
     
-    print(f'계좌번호 : {user["Account"]}원')
-    print(f'이름 : {user["Name"]}원')
-    print(f'잔액 : {user["Balance"]}원')
+    display_user_info(user, '\n')
 
     money = input("입금하실 금액을 입력해주세요 : ")
-
     # Error : 잘못된 money 입력
-    while not money.isdigit() or floor(int(money)) < 0:
+    while not money.isdigit() or int(money) < 0:
       print("입금하신 금액이 올바르지 않습니다.\n")
       money = input("예금 : ")
     
-    user["Balance"] += floor(int(money))
+    user["Balance"] += int(money)
     
     # Display
     print(f'##계좌잔고: {user["Balance"]} 원##')
@@ -77,6 +77,7 @@ class Bank():
   # =================== 출금 method ===================
   def withdraw(self):
     print("======출금 하기======")
+
     account = input("출금하실 계좌번호를 입력해주세요 : ")
     # Error : 잘못된 account 형식 전달
     while not account.isdigit():
@@ -84,30 +85,35 @@ class Bank():
       account = input("계좌번호 : ")
     
     user = (user for user in self.users if user['Account'] == account)
-
     # Error : users dict 내 찾는 user가 없는 경우
     if user == None:
       print("해당 계좌번호를 가진 사용자가 없습니다.")
       return-1
     
-    print(f'계좌번호 : {user["Account"]}')
-    print(f'이름 : {user["Name"]}')
-    print(f'잔액 : {user["Balance"]}')
+    display_user_info(user, '\n')
 
     money = input("출금하실 금액을 입력해주세요 : ")
-
     # Error : 잘못된 money 입력
-    while not money.isdigit() or floor(int(money)) < 0:
+    while not money.isdigit() or int(money) < 0:
       print("입금하신 금액이 올바르지 않습니다 : 음수 입력\n")
       money = input("예금 : ")
 
     
     # Error : 계좌 내 금액보다 더 많은 돈을 출금했을 경우
-    while user["Balance"] - floor(int(money)) < 0:
+    while user["Balance"] - int(money) < 0:
       print("계좌 내 금액보다 큰 값의 금액을 입력하셨습니다.")
       money = input("출금하실 잔액을 입력해주세요 : ")
 
-    
-    
-    
+    user["Balance"] -= int(money)
+
+    # Display
+    print(f'##계좌잔고: {user["Balance"]} 원##')
+    print("##출금이 완료되었습니다##")
+    print("================")
+
+  # =================== 조회 method ===================  
+  def inquiry(self):
+    for user in self.user:
+      display_user_info(user, ' / ')
+
     
